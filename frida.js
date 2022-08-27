f184431b78e878ed26c1e1c9c9aa630cafb32112
wzy <59381521+wzyyyyyyy@users.noreply.github.com>
2022-08-27 20:51:11 +0800

    Create frida.js

---
+++ frida.js
@@ -0,0 +1,12 @@
+const m = Module.findBaseAddress("libminecraftpe.so");
+const pattern = '10 a0 52 e0 03 13 aa e1 03 15 aa e2 03 16 aa';
+Memory.scan(m, 999999999999999, pattern, {
+  onMatch(address, size) {
+    Memory.patchCode(address, 1, code => { code.writeU8(0x40) } );
+    // Optionally stop scanning early:
+    return 'stop';
+  },
+  onComplete() {
+    common.dialog('Memory scan complete');
+  }
+});
